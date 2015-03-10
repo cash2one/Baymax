0c32021c3dce814f91fcefb00c1d2deae17bca39
guohai <guohai@live.com>
2015-03-10 18:16:48 +0800

    new

--- app/views.py
+++ app/views.py
@@ -5,17 +5,19 @@ from django.http import HttpResponse
 from django import forms
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
-from .models import Task
+from .models import Task, AppUser
+
 
 def index(request):
     tasks = Task.objects.all()
-    return render_to_response("app/index.html", {"tasks": tasks},context_instance=RequestContext(request))
-    #return render(request,'index.html')
+    return render_to_response("app/index.html", {"tasks": tasks}, context_instance=RequestContext(request))
+    # return render(request,'index.html')
+
 
 def about(request):
     # tasks = Task.objects.all()
-    #return render_to_response("resp.html", {"tasks": tasks})
-    return render(request, 'app/about.html',context_instance=RequestContext(request))
+    # return render_to_response("resp.html", {"tasks": tasks})
+    return render(request, 'app/about.html', context_instance=RequestContext(request))
 
 
 class TaskForm(forms.Form):
@@ -43,7 +45,7 @@ def task(request):
             task_Type = cd['TaskType']
             task_Count = cd['TaskCount']
             task_State = cd['TaskState']
-            creator = 1
+            creator = AppUser.objects.get(id=1)
             deviceType = cd['DeviceType']
             description = cd['Description']
             createTime = cd['CreateTime']
@@ -51,11 +53,12 @@ def task(request):
             createWay = cd['CreateWay']
             isShared = cd['IsShared']
             task = Task(TaskName=task_name, TaskType=task_Type, TaskCount=task_Count, TaskState=task_State,
-                        Creator=creator, DeviceType=deviceType, Description=description, CreateTime=createTime, UpdateTime=updateTime, CreateWay=createWay,
+                        Creator=creator, DeviceType=deviceType, Description=description, CreateTime=createTime,
+                        UpdateTime=updateTime, CreateWay=createWay,
                         IsShared=isShared)
 
             task.save()
-            #id = Task.objects.order_by('-publish_time')[0].id
+            # id = Task.objects.order_by('-publish_time')[0].id
             return HttpResponseRedirect('http://www.baidu.com')
     else:
         form = TaskForm()
