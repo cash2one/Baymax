308e4013a2a41236246197a9e904b35ba817662d
uuoai <guohai@live.com>
2015-03-11 00:24:13 +0800

    new

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ __author__ = 'guguohai@outlook.com'
 from django import forms
 
 class TaskForm(forms.Form):
-    TaskName = forms.CharField(max_length=50)
+    TaskName = forms.CharField(max_length=200)
     TaskType = forms.CharField(label='0')
     TaskCount = forms.CharField()
     TaskState = forms.CharField()
