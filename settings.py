fe4d7606ec069f05922ada31bf82fa4ccdf1e127
guohai <guohai@live.com>
2015-03-11 15:48:02 +0800

    new

--- settings.py
+++ settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testmanager',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'root2158',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
