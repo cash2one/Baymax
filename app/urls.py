7645d85b977120d53ee485801483f36e401e4780
guohai <guohai@live.com>
2015-03-12 13:08:58 +0800

    socket.io

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,8 @@ __author__ = 'guguohai@outlook.com'
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-                       url(r'^$', 'app.views.index'),
-                       url(r'^about/', 'app.views.about'),
-                       url(r'^task/', 'app.views.task'),
+    url(r'^$', 'app.views.index'),
+    url(r'^about/', 'app.views.about'),
+    url(r'^task/', 'app.views.task'),
 
 )
