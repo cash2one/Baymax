fd2195b24e5c62664f478f4078a1a1bc7d6f33a1
guohai <guohai@live.com>
2015-03-18 15:58:40 +0800

    add pages

--- app/tests.py
+++ app/tests.py
@@ -2,3 +2,11 @@ from django.test import TestCase
 
 # Create your tests here.
 
+strs='32671'
+
+for i in range(0,len(strs)):
+    aa=''
+    for k in range(i+1,len(strs)):
+        aa+=strs[k]
+
+    print strs[i],aa
--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'app.views.index'),
+    url(r'^home/', 'app.views.index'),
     url(r'^about/', 'app.views.about'),
     url(r'^task/', 'app.views.task'),
     url(r'^testcase/', 'app.views.test_case'),
