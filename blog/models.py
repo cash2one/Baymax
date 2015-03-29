015cb70f25054882f9324f6826b9d295bbbbe3e1
uuoai <guohai@live.com>
2015-03-29 22:45:19 +0800

    add blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    UserName = models.CharField(max_length=50)
+    DisplayName = models.CharField(max_length=50)
+    UserPassword = models.CharField(max_length=50)
+    Email = models.CharField(max_length=100, blank=True)
+    UserDefined = models.TextField(blank=True)
