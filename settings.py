996df3ce548b739ecce94a3d6f2f00e07483e293
guohai <guohai@live.com>
2015-03-31 11:13:45 +0800

    add RESTful

--- settings.py
+++ settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'baymax',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'root2158',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
