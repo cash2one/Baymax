d08ac011a527369d5de1ff47476dba49556c75d2
guohai <guohai@live.com>
2015-03-31 10:52:41 +0800

    add RESTful

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+__author__ = 'guguohai@pathbook.com.cn'
+
+from django import forms
+
+
+class ArticleForm(forms.Form):
+    Title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    Content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}))
+    Tags = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,54 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-import utils
-from .models import Article,ArticleType,Comment
+import utils, forms
+from .models import Article, ArticleType, Comment
 
 
 def home(request):
-    articles = Article.objects.all()
-    return render_to_response('blog/index.html', {"home": '', 'articles': articles},
+    bing=utils.getBingImg('http://cn.bing.com/')
+    arts = Article.objects.all()
+    # if request.method == 'POST':
+    #     form = forms.ArticleForm(request.POST)
+    #     if form.is_valid():
+    #         cd = form.cleaned_data
+    #         Title = cd['Title']
+    #         ArticleTypeId = ArticleType.objects.get(id=1)
+    #         Content = cd['Content']
+    #         ReplyNum = 3
+    #         ReadNum = 2
+    #         Property = 1
+    #         Tags = cd['Tags']
+    #
+    #         article = Article(Title=Title, ArticleTypeId=ArticleTypeId, Content=Content, ReplyNum=ReplyNum,
+    #                           ReadNum=ReadNum, Property=Property, Tags=Tags)
+    #         article.save()
+    # else:
+    #     print 'fewg'
+    #     form = forms.ArticleForm()
+    return render_to_response('blog/index.html', {'bing':bing,'arts': arts},
+                              context_instance=RequestContext(request))
+
+
+def post_article(request):
+    # return render_to_response("app/task.html", {"tasks": tasks},context_instance=RequestContext(request))
+    arts = Article.objects.all()
+    if request.method == 'POST':
+        form = forms.ArticleForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Title = cd['Title']
+            ArticleTypeId = 1
+            Content = cd['Content']
+            ReplyNum = 3
+            ReadNum = 2
+            Property = 1
+            Tags = cd['Tags']
+
+            article = Article(Title, ArticleTypeId, Content, ReplyNum, ReadNum, Property, Tags)
+            article.save()
+    else:
+        print 'fewg'
+        form = forms.ArticleForm()
+    return render_to_response('blog/index.html', {'arts': arts, 'form': form},
                               context_instance=RequestContext(request))
