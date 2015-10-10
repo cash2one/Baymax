3668f99be51d842bfbeff6bb3c6597954ac633df
hai <guohai@live.com>
2015-10-10 16:02:31 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'baymax',
         'USER': 'root',
-        'PASSWORD': 'ro0oT123',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
