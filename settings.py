a135bd663dbe3748157d56293daa464bbe33a2e0
guohai <guohai@live.com>
2015-11-16 00:21:46 +0800

    task form

--- settings.py
+++ settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'woodpecker',
 #         'USER': 'root',
-#         'PASSWORD': 'rOot@123',
+#         'PASSWORD': '',
 #         'HOST': '127.0.0.1',
 #         'PORT': '3306',
 #     }
